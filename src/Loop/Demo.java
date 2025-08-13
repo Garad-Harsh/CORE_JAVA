e57c6c0dfcf4453be8188888e8ffdfaff9da8940
Harshal Garad <harshalgarad2000@gmail.com>
2025-08-13 11:49:34 +0530

    Second Commit

---
+++ src/Loop/Demo.java
@@ -0,0 +1,37 @@
+package Loop;
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int input=sc.nextInt();
+	    System.out.println("Enter Your Input");
+	    
+	    
+	    if(input==1) {
+	    	System.out.println("Monday");
+	    }else if(input==2) {
+	    	System.out.println("Tuesday");
+	    }
+	    else if(input==3) {
+	    	System.out.println("Wed");
+	    }
+	    else if(input==4) {
+	    	System.out.println("Thu");
+	    }
+	    else if(input==5) {
+	    	System.out.println("Fri");
+	    }
+	    else if(input==6) {
+	    	System.out.println("Sat");
+	    }
+	    else {
+	    	System.out.println("Sunday");
+	    }
+	    
+	  
+
+	}
+
+}
---
+++ src/Loop/Demo1.java
@@ -0,0 +1,46 @@
+package Loop;
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Your Day");
+		String str=sc.next();
+		
+		
+		switch (str) {
+		case "Monday": {
+			System.out.println(1);
+			break;
+		}
+		case "Tuesday": {
+			System.out.println(2);
+			break;
+		}
+		case "Wednsday": {
+			System.out.println(3);
+			break;
+		}
+		case "Thursday": {
+			System.out.println(4);
+			break;
+		}
+		case"Friday": {
+			System.out.println(5);
+			break;
+		}
+		case "Saturday": {
+			System.out.println(6);
+			break;
+		}
+		case "Sunday": {
+			System.out.println(7);
+			break;
+		}
+		default:
+			System.out.println("Holiday");
+		}
+	}
+
+}
